1af99df1e6461cf1698ca4ec4514eaffc4adcc75
Sofia Mattiasson <mattiasson.sofia@gmail.com>
2023-04-18 21:05:33 +0200

    Add hamburger menu to the header

--- msb-client/src/main.ts
+++ msb-client/src/main.ts
@@ -3,3 +3,16 @@ import './scss/main.scss'
 
 // Import all of Bootstrap's JS
 import 'bootstrap/dist/js/bootstrap.js'
+
+
+const menu = document.querySelector('.menu')
+const ul = document.querySelector('ul')
+const hamburgerMenu = document.querySelector('.hamburger')
+const menuLink = document.querySelector('.menu-text')
+
+menu!.addEventListener('click', () => {
+  ul!.classList.toggle('show')
+  hamburgerMenu!.classList.toggle('close')
+
+  ul!.classList.contains('show') ? menuLink!.textContent = 'stäng' : menuLink!.textContent = 'meny'
+});
